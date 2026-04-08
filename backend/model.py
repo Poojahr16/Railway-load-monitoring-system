ffe04f3f60b46edb73780e28fb01d8299677844f
Poojahr16 <pb02sep2006@gmail.com>
2026-04-09 01:19:38 +0530

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,16 +1,2 @@
 def predict(data):
     return {"prediction": "success"}  # dummy response
-
-# load trained model
-model = tf.keras.models.load_model("../ml_model/model.h5", compile=False)
-
-def predict(load, vibration, temperature):
-
-    data = np.array([[load, vibration, temperature]])
-
-    prediction = model.predict(data)
-
-    if prediction[0][0] > 0.5:
-        return "Danger ⚠️ Bridge Failure Risk"
-    else:
-        return "Safe ✅"
