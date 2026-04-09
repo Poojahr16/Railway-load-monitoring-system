1f95bbfe9f09a5e77c6bb19e475f2f49ee76e530
Poojahr16 <pb02sep2006@gmail.com>
2026-04-09 10:17:33 +0530

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,2 +1,7 @@
 def predict(load, vibration, temperature):
-    return "working"
+
+    # Define thresholds (you can adjust later)
+    if load > 80 or temperature > 70 or vibration > 50:
+        return "DANGER ⚠️ Bridge Failure Risk"
+    else:
+        return "SAFE ✅"
